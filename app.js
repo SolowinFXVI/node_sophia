ee57f5e1be141f2333d400e825598caf116cab39
Julien JACQUET <julien.n.jacquet@protonmail.com>
2022-02-09 15:36:07 +0100

    removed obsolete files

--- app.js
+++ app.js
@@ -4,12 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 ;
 var app = express();
 // view engine setup
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
